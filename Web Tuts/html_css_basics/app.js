4dd879e9fc3144507c406bd49e5ad84f46045218
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-05 20:02:13 +0530

    Learn DOM

--- Web Tuts/html_css_basics/app.js
+++ Web Tuts/html_css_basics/app.js
@@ -1,7 +1,11 @@
-let btn = document.querySelector('.btn-clicked');
+let btn = document.querySelector('.btn');
 
-console.log(btn);
+// Array.from
+// pseudo-classes Also Work
 
-btn.addEventListener("click", ()=>{
-    console.log('Clicked');
-})
+btn.addEventListener('click', () => {
+    let li_last = document.querySelectorAll('ul li:last-child');
+    for(let li of li_last) {
+        console.log(li.textContent);
+    }
+});
