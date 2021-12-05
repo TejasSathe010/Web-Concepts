a2544414d7085158745efa998c4be1eb190ce3cc
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-05 21:02:59 +0530

    Learn DOM

--- Web Tuts/html_css_basics/app.js
+++ Web Tuts/html_css_basics/app.js
@@ -3,11 +3,8 @@ let btn = document.querySelector('.btn');
 // Array.from
 // pseudo-classes Also Work in querySelector
 // node.matches (True/False)
+// ele.closest Look for closest parent with given selector
 
 btn.addEventListener('click', () => {
-    for(let node of document.body.children) {
-        if(node.matches('a[href ^= "http"]')) {
-            console.log(node.href);
-        }
-    }   
+    console.log(btn.attributes);
 });
