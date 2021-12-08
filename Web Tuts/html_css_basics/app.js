c9b5d03fa62e4d56e8931cc7b9acd4d3ffb942e6
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-08 21:17:01 +0530

    Flex tuts

--- Web Tuts/html_css_basics/app.js
+++ Web Tuts/html_css_basics/app.js
@@ -1,6 +1,7 @@
 let add_btn = document.querySelector('.add-btn');
 let del_btn = document.querySelector('.del-btn');
-let ul = document.querySelector('.book')
+let ul = document.querySelector('.book');
+let input = document.querySelector('#name');
 
 // Array.from
 // pseudo-classes Also Work in querySelector
@@ -9,12 +10,23 @@ let ul = document.querySelector('.book')
 
 add_btn.addEventListener('click', () => {
     let li = document.createElement('li');
-    li.textContent = li.nodeType;
+    li.textContent = input.value;
     li.classList.add('click')
+    add_btn.setAttribute('name', input.value);
+
+    console.log(add_btn.getAttribute('name'));
+    add_btn.textContent = input.value
+
+    add_btn.setAttribute('disabled', 'disabled');
+
+    console.log(li.getAttribute('class'));
     ul.appendChild(li);
 });
 
 del_btn.addEventListener('click', () =>{
     ul.removeChild(ul.firstChild);
-})
+    for(let attr of del_btn.attributes) {
+        console.log(`${attr.name} = ${attr.value}` )  
+    }
+});
 
