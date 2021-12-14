f96d1151ab019334ac7358f9237ed118bd1d3436
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-14 22:22:41 +0530

    Modify react app

--- Web Tuts/React/blog-app/src/Home.js
+++ Web Tuts/React/blog-app/src/Home.js
@@ -1,13 +1,18 @@
-const Home = () => {
+import { useState } from 'react';
 
-    const handleClick = (name, event) => {
-        console.log('Clicked!!! ' + name);
+const Home = () => {
+    const [name, setName] = useState('Tejas');
+    const [age, setAge] = useState(23);
+    const handleClick = () => {
+        setName('Sathe');
+        setAge(24);
     };
 
     return ( 
         <div className="home">
             <h1>Home Page</h1>
-            <button onClick={() => handleClick('Tejas') }>Click me</button>
+            <p>{ name } is { age } years old.</p>
+            <button onClick={handleClick }>Click me</button>
         </div>
      );
 }
