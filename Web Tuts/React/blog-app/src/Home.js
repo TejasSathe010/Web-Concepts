4e3007b45860a95f2410e916905df221cd3b5053
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-28 23:19:50 +0530

    Create responsive layout

--- Web Tuts/React/blog-app/src/Home.js
+++ Web Tuts/React/blog-app/src/Home.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState()
+  const [blogs, setBlogs] = useState(null)
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -10,7 +10,14 @@ const Home = () => {
   }
 
   useEffect(() => {
-      console.log("Use effectRun!!!");
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        setBlogs(data);
+      })
   }, [])
 
   return (
