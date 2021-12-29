84438da098fe2531df3ca2c2fd2e145e1b3e83b2
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-29 22:23:28 +0530

    Update react app

--- Web Tuts/React/blog-app/src/App.js
+++ Web Tuts/React/blog-app/src/App.js
@@ -1,15 +1,30 @@
 import Navbar from './Navbar.js';
 import Home from './Home.js';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create.js';
+import BlogDetails from './BlogDetails.js';
 
 function App() {
   
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
+    <Router>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/create">
+                <Create />
+              </Route>
+              <Route path="/blogs/:id">
+                <BlogDetails />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+    </Router>
   );
 }
 
--- Web Tuts/React/blog-app/src/useFetch.js
+++ Web Tuts/React/blog-app/src/useFetch.js
@@ -7,8 +7,9 @@ const useFetch = (url) => {
     
     // Runs every render of component or State changes
     useEffect(() => {
+      const abortCont = new AbortController();
         setTimeout(() => {
-          fetch(url)
+          fetch(url, {signal: abortCont.signal})
           .then(res => {
             if(!res.ok) {
               throw new Error('Could not fetch blog data');
@@ -21,10 +22,15 @@ const useFetch = (url) => {
             setError(null);
           })
           .catch(err => {
-            setError(err.message);
-            setIsPending(false);
+            if(err.name === 'AbortError') {
+              console.log("Request Aborted!");
+            } else {
+              setError(err.message);
+              setIsPending(false);
+            }
           })
-        }, 2000);
+        }, 1000);
+        return () => {abortCont.abort()}
     }, [url]);
     return {data, isPending, error};
 }
