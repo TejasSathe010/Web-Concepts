bf4c64a367ec90c3baaf0a6d689fe8a5b08ec209
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-30 16:54:45 +0530

    Complete react app

--- Web Tuts/React/blog-app/src/Create.js
+++ Web Tuts/React/blog-app/src/Create.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-    return (
-        <div className="Create">
-            <h2>Add a New Blog</h2>
-        </div>
-    )
-}
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
-export default Create
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        setIsPending(true);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("New Blog Added!");
+            setIsPending(false);
+            // history.go(-1);
+            history.push('/');
+        });
+    }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        { !isPending && <button>Add Blog</button> }
+        { isPending && <button disabled>Adding Blog...</button> }
+      </form>
+    </div>
+  );
+}
+ 
+export default Create;
+
