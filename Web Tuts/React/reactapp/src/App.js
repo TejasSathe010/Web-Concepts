9d6d770d33d349dd6fdca6ca165619c3d9448451
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-31 21:48:07 +0530

    Add form input

--- Web Tuts/React/reactapp/src/App.js
+++ Web Tuts/React/reactapp/src/App.js
@@ -1,36 +1,56 @@
 import Header from './Header';
+import SearchItem from './SearchItem';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
 
 function App() {
 
-  const [items, setItems] = useState([
-    {
-        id: 1,
-        checked: true,
-        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-        id: 2,
-        checked: false,
-        item: "Item 2"
-    },
-    {
-        id: 3,
-        checked: false,
-        item: "Item 3"
-    }
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+
+  const [newItem, setNewItem] = useState('');
+
+  const [search, setSearch] = useState();
+
+
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length-1].id + 1 : 0;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!newItem) { return; }
+    addItem(newItem);
+    setNewItem('');
+  }
 
   return (
     <div className="App">
-      <Header title="Tejas's List"/>
-      <Content
-        items = { items }
-        setItems = { setItems }
+      <Header title="Grocery List"/>
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
       />
-      <Footer 
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
+      <Content
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        setItems={setItems}
+      />
+      <Footer
         length = { items.length }
       />
     </div>
