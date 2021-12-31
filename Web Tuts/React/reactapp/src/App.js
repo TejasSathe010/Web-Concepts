38be00752a681a26520eee148918526c8194c875
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-31 18:02:31 +0530

    Add props property

--- Web Tuts/React/reactapp/src/App.js
+++ Web Tuts/React/reactapp/src/App.js
@@ -1,14 +1,38 @@
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import { useState } from 'react';
 
 function App() {
 
+  const [items, setItems] = useState([
+    {
+        id: 1,
+        checked: true,
+        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+    },
+    {
+        id: 2,
+        checked: false,
+        item: "Item 2"
+    },
+    {
+        id: 3,
+        checked: false,
+        item: "Item 3"
+    }
+  ]);
+
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Tejas's List"/>
+      <Content
+        items = { items }
+        setItems = { setItems }
+      />
+      <Footer 
+        length = { items.length }
+      />
     </div>
   );
 }
